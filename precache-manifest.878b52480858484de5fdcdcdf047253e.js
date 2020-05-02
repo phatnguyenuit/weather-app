ecd0692571b98a03a237ccd1a4f7a8e076452370
Nguyen Phat <phatnt.uit@gmail.com>
2020-05-02 10:11:19 +0700

    Updates

---
+++ precache-manifest.878b52480858484de5fdcdcdf047253e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb387ec40d149c5184ddc95b420f1fb5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "677c6adc349b1506c1df",
+    "url": "/weather-app/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "8d401cfbde05ceda1ec2",
+    "url": "/weather-app/static/js/2.d768aac4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.d768aac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "677c6adc349b1506c1df",
+    "url": "/weather-app/static/js/main.ee1435ad.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  }
+]);
